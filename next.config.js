421f9ad88a60cf8977175466bf2afdb0ad3cecf5
FaberJoo <79781818+FaberJoo@users.noreply.github.com>
2022-06-26 17:48:18 +0900

    Revert "Login 페이지 추가 및 카카오 로그인"

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	compiler: {
-		// ssr and displayName are configured by default
-		styledComponents: true,
-	},
+	reactStrictMode: true,
 	images: {
-		loader: 'akamai',
-		path: '/',
-	},
-	env: {
-		CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_KEY,
-		REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
-		API_DEV: process.env.NEXT_PUBLIC_API_DEV,
+		loader: 'imgix',
+		path: 'http://localhost:3000',
 	},
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
 import '../styles/globals.css';
-import { Amplify } from 'aws-amplify';
-import awsmobile from '..//aws-exports';
-Amplify.configure({ ...awsmobile, ssr: true });
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
